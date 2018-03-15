18a19bf6f765fa5c20a8a52482aa3371c3041110
chenjunhong <chenjunhong@useease.com>
2018-03-15 15:30:25 +0800

    自动修改变量

--- Dockerfile
+++ Dockerfile
@@ -3,32 +3,33 @@ MAINTAINER chenjunhong@useease.com
 
 ## The below part is all the env need to config the server ,for the default value 
 
-ENV server /argus-web/server
-ENV OPENTSDBHOST "192.168.0.253"
-ENV OPENTSDBPORT 4242
+# ENV server /argus-web/server
+# ENV OPENTSDBHOST ""
+# ENV OPENTSDBPORT 0
 #  Argus Collector Manager settings
-ENV CMHOST "192.168.0.253"
-ENV CMPORT 8081
+# ENV CMHOST ""
+# ENV CMPORT 8081
 #  Argus tracing related settings
-ENV CHAINHOST "192.168.0.253"
-ENV CHAINPORT 9999
+# ENV CHAINHOST "127.0.0.1"
+# ENV CHAINPORT 9999
 #  MONGODB related settings
-ENV MONGOHOST "192.168.0.253"
-ENV MONGOPORT 27017
-ENV MONGOPASSWD ''
-ENV MONGOUSER   ''
+# ENV MONGOHOST "127.0.0.1"
+# ENV MONGOPORT 27017
+# ENV MONGOPASSWD ''
+# ENV MONGOUSER   ''
 #  REDIS setiings
-ENV REDISHOST  "192.168.0.253"
-ENV REDISPORT  6379
-ENV REDISPASSWD  ''
-ENV SESSIONDB 4 
+# ENV REDISHOST  "127.0.0.1"
+# ENV REDISPORT  6379
+# ENV REDISPASSWD  ''
+# ENV SESSIONDB 4 
 #  WECHAT RELATED settings   
 ## if want to use wechat related server ,this should be use with a not private ip address
-ENV WECHATREDISHOST "192.168.0.253"
-ENV WECHATREDISPORT 6379 
-ENV WECHATREDISPASSWD ""
-ENV WECHATREDISCHAN ""
-ENV WECHATQRCODEURL ""
+# ENV WECHATREDISHOST "127.0.0.1"
+# ENV WECHATREDISPORT 6379 
+# ENV WECHATREDISPASSWD ""
+# ENV WECHATREDISCHAN ""
+# ENV WECHATQRCODEURL ""
+# ENV WECHATREDISDB 2
 
 
 RUN mkdir -p /argus-web/server
@@ -39,8 +40,6 @@ WORKDIR $server
 RUN  npm install -g pm2 \
       && npm install
 RUN cat start_config.js 
-#WORKDIR $phoneapp
-## TODO : wait for the app_phone to fix the error for build  
-# TOOD write shell script to replace the start_config for express server
 EXPOSE 8080
-CMD ['npm','run','start']
+# CMD ['npm','run','start']
+ENTRYPOINT [ "entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+##TODO to replace the config variable to other 
+setting="$@"
+
+# TO generate the default value 
+if [[ ! -n $OPENTSDBHOST ]]; then
+	export OPENTSDBHOST="127.0.0.1"
+fi
+
+
+if [[ ! -n $OPENTSDBPORT ]]; then
+	export OPENTSDBPORT=4242
+fi
+
+if [[ ! -n $CMHOST ]]; then
+	export CMHOST="127.0.0.1"
+fi
+
+if [[ ! -n $CMPORT ]]; then
+	export CMPORT=8001
+fi
+
+if [[ ! -n $CHAINHOST ]]; then
+    
+	export CHAINHOST='127.0.0.1'
+fi
+
+if [[ ! -n $CHAINPORT ]]; then
+    
+	export CHAINPORT=9999
+fi
+
+if [[ ! -n $MONGOHOST ]]; then
+    
+	export MONGOHOST='127.0.0.1'
+fi
+
+if [[ ! -n $MONGOPORT ]]; then
+    
+	export MONGOPORT=27017
+fi
+
+if [[ ! -n $MONGOPASSWD ]]; then
+    
+	export MONGOPASSWD=""
+fi
+
+if [[ ! -n $MONGOUSER ]]; then
+    
+	export MONGOUSER=""
+fi
+
+if [[ ! -n $REDISHOST ]]; then
+    
+	export REDISHOST='127.0.0.1'
+fi
+
+if [[ ! -n $REDISPORT ]]; then
+    
+	export REDISPORT=6379
+fi
+
+if [[ ! -n $REDISPASSWD ]]; then
+    
+	export REDISPASSWD=''
+fi
+
+if [[ ! -n $SESSIONDB ]]; then
+    
+	export SESSIONDB=4
+fi
+
+if [[ ! -n $WECHATREDISHOST ]]; then
+    
+	export WECHATREDISHOST='127.0.0.1'
+fi
+
+if [[ ! -n $WECHATREDISPORT ]]; then
+    
+	export WECHATREDISPORT=6379
+fi
+
+if [[ ! -n $WECHATREDISPASSWD ]]; then
+    
+	export WECHATREDISPASSWD=''
+fi
+
+if [[ ! -n $WECHATREDISCHAN ]]; then
+    
+	export WECHATREDISCHAN=''
+fi
+
+if [[ ! -n $WECHATQRCODEURL ]]; then
+    
+	export WECHATQRCODEURL=''
+fi
+
+if [[ ! -n $WECHATREDISDB ]]; then
+    
+	export WECHATREDISDB=2
+fi
+
+if [[ ! -n $WECHATAPIHOST ]]; then
+    
+	export WECHATAPIHOST='127.0.0.1'
+fi
+
+#### To Replace  the config value of start_config.js
+cd /argus-web/server
+
+sed -e "s#OPENTSDBHOST#${OPENTSDBHOST}#" \
+    -e "s#OPENTSDBPORT#${OPENTSDBPORT}#" \
+    -e "s#CMHOST#${CMHOST}#" \
+    -e "s#CMPORT#${CMPORT}#" \
+    -e "s#CHAINHOST#${CHAINHOST}#" \
+    -e "s#CHAINPORT#${CHAINPORT}#" \
+    -e "s#MONGOHOST#${MONGOHOST}#" \
+    -e "s#MONGOPORT#${MONGOPORT}#" \
+    -e "s#MONGOPASSWD#${MONGOPASSWD}#" \
+    -e "s#MONGOUSER#${MONGOUSER}#" \
+    -e "s#REDISHOST#${REDISHOST}#" \
+    -e "s#REDISPORT#${REDISPORT}#" \
+    -e "s#REDISPASSWD#${REDISPASSWD}#" \
+    -e "s#WECHATREDISHOST#${WECHATREDISHOST}#" \
+    -e "s#WECHATREDISPORT#${WECHATREDISPORT}#" \
+    -e "s#WECHATREDISDB#${WECHATREDISPASSWD}#" \
+    -e "s#WECHATREDISPASSWD#${WECHATREDISPASSWD}#" \
+    -e "s#WECHATAPIHOST#${WECHATAPIHOST}#" \
+    -e "s#WECHATQRCODEURL#${WECHATQRCODEURL}#" \
+    -e "s#WECHATREDISCHAN#${WECHATREDISCHAN}#" \
+    -e "s#SESSIONDBNUM#${SESSIONDB}#" \
+     start_config.js > start_config2.js
+
+mv start_config2.js start_config.js 
+
+## hold the node server for long loop without pm2 holding 
+node ./bin/www.js
